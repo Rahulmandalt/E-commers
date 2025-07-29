d7db9d0e808cdcfcffd6a039a25fc976956b9e8d
Rahulmandalt <168030823+Rahulmandalt@users.noreply.github.com>
2025-07-29 18:44:38 +0530

    Add files via upload
    E-commers-api

---
+++ data.js
@@ -0,0 +1,108 @@
+ export const items = [
+    {
+        id:1,
+category:"Tshirts",
+title:"Men Checkered Polo Neck Cotton Blend Dark Blue, White T-Shirt",
+img: "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/k/4/k/l-ausk0578-ausk-original-imahcw5fwvmzht8v.jpeg?q=70",
+price:"250"
+    },
+    {
+        id:2,
+category:"Tshirts",
+title:"Men Geometric Print Polo Neck Pure Cotton Black T-Shirt",
+img:"https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/s/2/t/l-men-temu-polo-001-rizim-original-imahbdjcf54jzdhe.jpeg?q=70",
+price: "332",
+    },
+    {
+        id:3,
+category:"Tshirts",
+title:"Men Solid Polo Neck Polyester Yellow T-Shirt",
+img:"https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/k/k/p/3xl-ts36-vebnor-original-imahbyg9czyps9xf.jpeg?q=70",
+price:"399"
+    },
+    {
+        id:4,
+category:"Tshirts",
+title:"Men Printed Round Neck Polycotton Green T-Shirt",
+img:"https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/4/v/j/xxl-oversizetsrt-101-kajaru-original-imagpyfaywwq2hfv.jpeg?q=70",
+price:"203",
+    },{
+        id:5,
+category:"Tshirts",
+title:"Men Solid Round Neck Polyester Black T-Shirt",
+img:"https://m.media-amazon.com/images/I/71Bt9DdAhFL._SY879_.jpg",
+price:"304",
+    },
+    {
+        id:6,
+category: "jeans",
+title: "Men Relaxed Fit Mid Rise Dark Blue Jeans",
+img: "https://rukminim2.flixcart.com/image/612/612/xif0q/jean/9/p/i/34-z3-mjw-dark-blue-zaysh-original-imah8ra6sgn8hf79.jpeg?q=70",
+price: "500",
+},
+{
+    id:7,
+category:  "jeans",
+title: "Peter England Men's Tapered Fit Mid Rise Classic Tape Stretchable Twill Jeans",
+img: "https://m.media-amazon.com/images/I/71nyeyGqZHL._SY879_.jpg",
+price: "999"
+},
+
+{
+    id:8,
+category: "jeans",
+title: "GRECIILOOKS Men High Rise Stylish Jeans | Jeans Regular Fit | Non Stretchable Jeans",
+img: "https://m.media-amazon.com/images/I/61aYYeb6O0L._SY879_.jpg",
+price: "799",
+},
+{
+    id:9,
+category: "jeans",
+title: "TAGAS Men Relaxed FIT Denim Jeans | Loose Jeans for Men | Mens Baggy Fit Denim Jeans Pant | MDJ-22",
+img: "https://m.media-amazon.com/images/I/51yBtgTlleL._SY879_.jpg",
+price: "899",
+},
+{
+    id:10,
+category:"kurti",
+title:"PARTHVI|Women's Pure Cotton Printed with Handwork Kurta with Pants and Dupatta Set | Ethnic Set | Suit Set | Kurta Set | Wedding Suit Set",
+img:"https://m.media-amazon.com/images/I/61akGUjKw8L._AC_UL480_FMwebp_QL65_.jpg",
+price:"759",
+},
+
+{
+    id:11,
+category:"kurti",
+title:"Arayna Women's Pure Cotton Bandhani Print Ethnic Kurta Set with Palazzo Pants & Dupatta",
+img: "https://m.media-amazon.com/images/I/71POfKfs6sL._SY879_.jpg",
+price: "999"
+},
+{
+    id:12,
+category: "kurti",
+title: "Arayna Women's Pure Cotton Bandhani Print Ethnic Kurta Set with Palazzo Pants & Dupatta",
+img: "https://m.media-amazon.com/images/I/71POfKfs6sL._SY879_.jpg",
+price: "990"
+},
+{
+id:13,
+category: "kurti",
+title: "GoSriKi Women's Rayon Viscose Straight Solid Kurta with Pant & Dupatta",
+img: "https://m.media-amazon.com/images/I/61ad1xAy4qL._SY879_.jpg",
+price:"499",
+},
+{
+id:14,
+category:"kurti",
+title: "rytras Women's Pure Cotton Printed A-Line Kurta Set with Palazzo Pants - Ethnic Wear, Maroon",
+img: "https://m.media-amazon.com/images/I/71kKTByrNQL._SY879_.jpg",
+price: "599",
+},
+{
+    id:15,
+category: "sheos",
+title: "BRUTON EVA Lite Sport Shoes Running Shoes for Men- White",
+img: "https://m.media-amazon.com/images/I/61md3szAgLL._SY695_.jpg",
+price:"499",
+}
+ ]
